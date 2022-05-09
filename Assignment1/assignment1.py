049460dc7af8c7bf96a1390c829a689105b2b4df
Martin Aimé Schepers <81292024+mschepers188@users.noreply.github.com>
2022-05-09 16:55:34 +0200

    DownloadsPMIDsfrompubmed

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,9 +1,22 @@
 import multiprocessing as mp
+from Bio import Entrez
 
+def querylister(query):
+    Entrez.email = "A.N.Other@example.com"     # Always tell NCBI who you are
+    handle = Entrez.esearch(db="pubmed", term=f"{query}[title]", retmax="10" )
+    record = Entrez.read(handle)
+    return record["IdList"]
 
-def lesson1():
-    pass
-
+def efetcher(id):
+    Entrez.email = "A.N.Other@example.com"  # Always tell NCBI who you are
+    handle = Entrez.efetch(db="pubmed", id=id, retmode="xml")  # rettype="gb", retmode="text"
+    return handle.read()
 
 if __name__ == "__main__":
-    print("Hello world!")
+    querylist = querylister('COVID')
+    # xmlout = efetcher(querylist)
+    # print(xmlout)
+    cpus = mp.cpu_count()
+    with mp.Pool(cpus) as pool:
+        results = pool.map(efetcher, querylist)
+    print(results)
