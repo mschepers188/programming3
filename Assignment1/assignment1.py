4089bc6b742393d2f755479224d1b1de28124797
Martin Aimé Schepers <81292024+mschepers188@users.noreply.github.com>
2022-05-11 22:01:42 +0200

    Links&Fetches
    Links and fetches, cannot get the pool.map to work at all.

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,22 +1,38 @@
-import multiprocessing as mp
+from unittest import result
 from Bio import Entrez
+from multiprocessing.dummy import Pool
+from multiprocessing import cpu_count
 
-def querylister(query):
-    Entrez.email = "A.N.Other@example.com"     # Always tell NCBI who you are
-    handle = Entrez.esearch(db="pubmed", term=f"{query}[title]", retmax="10" )
-    record = Entrez.read(handle)
-    return record["IdList"]
+class BioDownloader:
+    
+    def elinker(id):
+        Entrez.email = "m.professional188@gmail.com"  # Tells pubmed who I am
+        handle = Entrez.elink(dbfrom="pubmed", id=id, linkname="pubmed_pubmed")  # creates handle for search
+        record = Entrez.read(handle)  # reads handle
+        handle.close()  # closes handle
+        linked = [link["Id"] for link in record[0]["LinkSetDb"][0]["Link"]]  # generates list with references
+        return linked  # returns list with references
 
-def efetcher(id):
-    Entrez.email = "A.N.Other@example.com"  # Always tell NCBI who you are
-    handle = Entrez.efetch(db="pubmed", id=id, retmode="xml")  # rettype="gb", retmode="text"
-    return handle.read()
+    def efetcher(id):
+        Entrez.email = "m.professional188@gmail.com"
+        handle = Entrez.efetch(db="pubmed", id=id, rettype="gb", retmode="xml", )
+        record = Entrez.read(handle)
+        return record
+
+
+    def multi_predict(predict, X):
+        pool = Pool(cpu_count())
+        results = pool.map(predict, X)
+        pool.close()
+        pool.join()
+        return results
 
 if __name__ == "__main__":
-    querylist = querylister('COVID')
-    # xmlout = efetcher(querylist)
-    # print(xmlout)
-    cpus = mp.cpu_count()
-    with mp.Pool(cpus) as pool:
-        results = pool.map(efetcher, querylist)
-    print(results)
+    biodownloader = BioDownloader
+    references = biodownloader.elinker('30577416')[0:10]  # grabs 10 first references
+    result = BioDownloader.multi_predict(biodownloader.efetcher, references)
+    
+    # print(biodownloader.efetcher(references))
+    # cpus = mp.cpu_count()
+    # with mp.Pool(cpus) as pool:
+    #     results = pool.map(efetcher, querylist)
