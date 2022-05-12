13e797082593ffb0e84ef2b613c6f85828a50152
maschepers <m.professional188@gmail.com>
2022-05-12 22:49:34 +0200

    ProgramDone
    Accepts sys.arg with parser, uses that input, searches and creates xml files in folder script is run from.

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -2,35 +2,58 @@ from Bio import Entrez
 from multiprocessing.dummy import Pool
 from multiprocessing import cpu_count
 import time
+import os
+import argparse as ap
+
 
 class BioDownloader:
-    
-    def elinker(id):
-        Entrez.email = "m.professional188@gmail.com"  # Tells pubmed who I am
-        results = Entrez.read(Entrez.elink(dbfrom="pubmed", db="pmc", LinkName="pubmed_pmc_refs", id=id, api_key='5fac5151cab100c58c572d349e388975b408'))
+
+    def __init__(self, email, api):
+        self.email = email
+        self.api_key = api
+        if os.path.exists('output'):
+            pass
+        else:
+            os.makedirs('output')
+
+    def elinker(self, id):
+        Entrez.email = self.email  # Tells pubmed who I am
+        results = Entrez.read(Entrez.elink(dbfrom="pubmed",
+                                           db="pmc",
+                                           LinkName="pubmed_pmc_refs",
+                                           id=id,
+                                           api_key=self.api_key))
         references = [f'{link["Id"]}' for link in results[0]["LinkSetDb"][0]["Link"]]
         return references
 
-
-    def efetcher(id):
+    def efetcher(self, id):
         time.sleep(1)
-        print('sleeping...')
-        Entrez.email = "m.professional188@gmail.com"
-        handle = Entrez.efetch(db="pmc", id=id, rettype="XML", retmode="text", api_key='5fac5151cab100c58c572d349e388975b408')
+        Entrez.email = self.email
+        handle = Entrez.efetch(db="pmc",
+                               id=id,
+                               rettype="XML",
+                               retmode="text",
+                               api_key=self.api_key)
         with open(f'output/{id}.xml', 'wb') as file:
             file.write(handle.read())
-        print('file created')
 
-
-    def multi_predict(predict, X):
+    def multi_predict(self, predict, x):
         pool = Pool(cpu_count())
-        pool.map(predict, X)
+        pool.map(predict, x)
         pool.close()
-        # pool.join()
 
 
 if __name__ == "__main__":
-    biodownloader = BioDownloader
-    references = biodownloader.elinker('30577416')[0:10]  # grabs 10 first references
-    print(references)
-    BioDownloader.multi_predict(biodownloader.efetcher, references)
+    argparser = ap.ArgumentParser(
+        description="Script that downloads (default) 10 articles referenced by the given PubMed ID concurrently.")
+    argparser.add_argument("-n", action="store",
+                           dest="n", required=False, type=int,
+                           help="Number of references to download concurrently.")
+    argparser.add_argument("pubmed_id", action="store", type=str, nargs=1,
+                           help="Pubmed ID of the article to harvest for references to download.")
+    args = argparser.parse_args()
+    print("Getting: ", args.pubmed_id)
+
+    biodownloader = BioDownloader("m.professional188@gmail.com", '5fac5151cab100c58c572d349e388975b408')
+    references = biodownloader.elinker(args.pubmed_id)[0:10]  # grabs 10 first references
+    biodownloader.multi_predict(biodownloader.efetcher, references)
