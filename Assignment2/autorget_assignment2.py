d073d3a34be991f42c8684ab7429e72034093869
maschepers <m.professional188@gmail.com>
2022-05-19 21:11:59 +0200

    AddedArgparser
    Argparser is not complete yet, plus still have to figure out how to use multiple PCs to perform the task. I also do not understand the usage terms yet...

--- Assignment2/autorget_assignment2.py
+++ Assignment2/autorget_assignment2.py
@@ -1,7 +1,7 @@
 from Bio import Entrez, Medline
-import pandas as pd
 import pickle
 import os
+import argparse as ap
 
 class PMgetter:
     def __init__(self, email, api):
@@ -13,33 +13,58 @@ class PMgetter:
         else:
             os.makedirs('output')
 
-    def elinker(self, id, refnum):
+    def elinker(self, pmid, numchildren=10):
         Entrez.email = self.email  # Tells pubmed who I am
         results = Entrez.read(Entrez.elink(dbfrom="pubmed",
                                            db="pmc",
                                            LinkName="pubmed_pmc_refs",
-                                           id=id,
+                                           id=pmid,
                                            api_key=self.api_key))
         references = [f'{link["Id"]}' for link in results[0]["LinkSetDb"][0]["Link"]]
-        return references[0:refnum]
+        return references[0:numchildren]
 
     def get_autors(self, pmid):
-        # pmid = '19304878'
-        Entrez.email = "m.professional188@gmail.com"     # Always tell NCBI who you are
-        handle = Entrez.efetch(db="pubmed",id=pmid,rettype="medline",retmode="text")
-        records = Medline.read(handle)
+        Entrez.email = self.email     # Always tell NCBI who you are
+        handle = Entrez.efetch(db="pubmed",
+                               id=pmid,
+                               rettype="medline",
+                               retmode="text",
+                               api_key=self.api_key)
 
-        autors = tuple(records['AU'])
-        
-        print(autors)
-        
-        # with open('tuplePickle.pkl','wb') as f: 
-        #     pickle.dump(autors, f)
-            # print("The tuple is pickled successfully.")
+        authors = Medline.read(handle)['AU']  # Gets the records with read and selects the authors
+
+        with open(f'output/{pmid}.authors.pickle', 'wb') as f:
+            pickle.dump(authors, f)
+            print(f"{pmid} authors pickled successfully.")
 
 
 if __name__ == "__main__":
+    # Instanciating argparser
+    argparser = ap.ArgumentParser(
+        description="Script that downloads (default) 10 articles referenced by the given PubMed ID "
+                    "concurrently and extracts their authors.")
+    argparser.add_argument("-n num", "--number_peons",
+                           action="store_true",
+                           dest="peons",
+                           help="Number of peons per client.")
+    argparser.add_argument("pubmed_id",
+                           action="store",
+                           type=str,
+                           nargs=1,
+                           help="Pubmed ID of the article to harvest for references to download.")
+    args = argparser.parse_args()
+    print("Args:", args)
+    print("Getting child references: ", args.pubmed_id)
+
     pmgetter = PMgetter("m.professional188@gmail.com", '5fac5151cab100c58c572d349e388975b408')
-    refs = pmgetter.elinker('19304878', 10)
+    # example pmid = '19304878'
+
+    refs = pmgetter.elinker(args.pubmed_id, 10)
     print(refs)
-    # pmgetter.get_autors('19304878')
+    for i in refs:
+        pmgetter.get_autors(i)
+
+# assignment2.py -n <number_of_peons_per_client>
+# [-c | -s] --port <portnumber>
+# --host <serverhost>
+# -a <number_of_articles_to_download> STARTING_PUBMED_ID
