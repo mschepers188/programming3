0837fb621b455a8824f73e08ee339d19deb5c2fb
Martin Schepers <maschepers@bioinf.nl>
2022-05-31 10:13:24 +0200

    Corrected using pylint

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -1,3 +1,8 @@
+"""
+Takes a input file created by assignment3.sh and extracts values
+for cores and time and plots them.
+output: timings.png
+"""
 import matplotlib.pyplot as plt
 
 
@@ -6,17 +11,21 @@ def time_plotter(file):
     Takes a input file and extracts cores and time, separated by ":"
     Creates a timings.png containing the values.
     """
-    
+
+    # creates list to hold threads and timings
     threads_timings = []
 
-    with open(file) as f:
-        for line in f:
+    # opens file and extracts values
+    with open(file, 'r', encoding="utf8") as f_vals:
+        for line in f_vals:
             if line[0].isdigit():
                 threads_timings.append(line.strip().split(":"))
-    
+
+    # separate values through list comprehension
     timing = [i[0] for i in threads_timings]
     threads = [i[1] for i in threads_timings]
 
+    # plot values and save to file
     plt.scatter(timing, threads)
     plt.title("Timings per number of threads")
     plt.savefig('output/timings.png')
