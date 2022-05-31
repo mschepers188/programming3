9ae76676e1b14fe6cb47d4e4162fdc4227b56e88
Martin Schepers <maschepers@bioinf.nl>
2022-05-31 10:37:11 +0200

    Corrected # input

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=m.a.schepers@st.hanze.nl
+#SBATCH --time 4:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
 #SBATCH --job-name=assignment3_maschepers
+#SBATCH --partition=assemblix
+
 export BLASTDB=/local-fs/datasets/
 
 mkdir -p output
