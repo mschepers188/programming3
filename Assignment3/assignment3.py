2b110b5ea2c7f588185d77dd84a3d4374412589e
Martin Schepers <maschepers@bioinf.nl>
2022-05-31 09:58:35 +0200

    Finished, use sbatch to run quickly

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -1,1 +1,25 @@
-print("Hello world")
+import matplotlib.pyplot as plt
+
+
+def time_plotter(file):
+    """
+    Takes a input file and extracts cores and time, separated by ":"
+    Creates a timings.png containing the values.
+    """
+    
+    threads_timings = []
+
+    with open(file) as f:
+        for line in f:
+            if line[0].isdigit():
+                threads_timings.append(line.strip().split(":"))
+    
+    timing = [i[0] for i in threads_timings]
+    threads = [i[1] for i in threads_timings]
+
+    plt.scatter(timing, threads)
+    plt.title("Timings per number of threads")
+    plt.savefig('output/timings.png')
+
+if __name__ == "__main__":
+    time_plotter('output/timings.txt')
--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=16
+#SBATCH --job-name=assignment3_maschepers
 export BLASTDB=/local-fs/datasets/
 
 mkdir -p output
 
-for n in {25..26}
+for n in {1..16}
 do /usr/bin/time -o output/timings.txt --append -f "${n}:%e" blastp -query MCRA.faa -db refseq_protein/refseq_protein -num_threads $n -outfmt 6 >> blastoutput.txt
 done
+
+python3 assignment3.py
