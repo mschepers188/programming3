5f5873aee5c0d530d53eb2800a695fb6a5f808bc
Martin Schepers <maschepers@bioinf.nl>
2022-05-31 17:16:08 +0200

    Made into one-liner

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -11,8 +11,6 @@ export BLASTDB=/local-fs/datasets/
 
 mkdir -p output
 
-for n in {1..16}
-do /usr/bin/time -o output/timings.txt --append -f "${n}:%e" blastp -query MCRA.faa -db refseq_protein/refseq_protein -num_threads $n -outfmt 6 >> blastoutput.txt
-done
+for n in {1..16}; do /usr/bin/time -o output/timings.txt --append -f "${n}:%e" blastp -query MCRA.faa -db refseq_protein/refseq_protein -num_threads $n -outfmt 6 >> blastoutput.txt; done
 
 python3 assignment3.py
