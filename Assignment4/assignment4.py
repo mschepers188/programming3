8b02acf9d85b23cfab73757d2294d78148b25153
Martin Schepers <maschepers@bioinf.nl>
2022-06-08 13:52:00 +0200

    Working with 2 values

--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -1,1 +1,38 @@
 # Check SeqIO for parsing .fa files and calculating the N50
+
+from Bio import SeqIO
+import sys
+
+FileIn = '/homes/maschepers/Documents/GithubRepos/programming3/Assignment4/output/contigs.fa'
+
+def contig_parser(file):
+    handle = open(file, 'r')
+    SeqRecords = SeqIO.parse(handle, 'fasta')
+    len_list = []  # create list to store lengths
+    for record in SeqRecords:   # loop through each fasta entry
+        length = len(record.seq)    # get sequence length
+        # print(length)  # print("%s: %i bp" % (record.id, length))
+        len_list.append(length)
+
+
+def calculate_N50(list_of_lengths):
+    """
+    Calculate N50 for a sequence of numbers.
+    Args:list_of_lengths (list): List of numbers.
+    Returns:float: N50 value.
+    """
+    tmp = []
+    for tmp_number in set(list_of_lengths):
+            tmp += [tmp_number] * list_of_lengths.count(tmp_number) * tmp_number
+    tmp.sort()
+ 
+    if (len(tmp) % 2) == 0:
+        median = (tmp[int(len(tmp) / 2) - 1] + tmp[int(len(tmp) / 2)]) / 2
+    else:
+        median = tmp[int(len(tmp) / 2)]
+ 
+    return median
+
+
+lenghts = contig_parser(FileIn)
+print(calculate_N50(lenghts))
--- Assignment4/assignment4.sh
+++ Assignment4/assignment4.sh
@@ -9,10 +9,11 @@
 
 export FILE1=/data/dataprocessing/MinIONData/MG5267/MG5267_TGACCA_L008_R1_001_BC24EVACXX.filt.fastq
 export FILE2=/data/dataprocessing/MinIONData/MG5267/MG5267_TGACCA_L008_R2_001_BC24EVACXX.filt.fastq
+export OUTPUT=/students/2021-2022/master/Martin_DSLS
 
-# Run velveth and velvetg
-velveth output 21 -fastq -short $FILE1 -shortPaired $FILE2
-velvetg output -exp_cov auto -cov_cutoff auto
+# Run velveth and store output in student folder 
+parallel -j10 'velveth ${OUTPUT}/output_${} ${} -fastq -short $FILE1 -shortPaired $FILE2' ::: 23 25
+# Run velvetg
+parallel -j10 'velvetg ${OUTPUT}/output_${} -exp_cov auto -cov_cutoff auto' ::: 23 25
 
-# mkdir -p output
-
+# Run python file and save output
