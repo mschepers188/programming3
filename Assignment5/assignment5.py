70acb60fd9c8f2c08b4e7c541795c928eabea28f
Martin Schepers <maschepers@bioinf.nl>
2022-06-17 15:46:02 +0200

    At question 3 using a subset

--- Assignment5/assignment5.py
+++ Assignment5/assignment5.py
@@ -1,11 +1,30 @@
 from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
 
-path = 'all_bacilli.tsv'
 
-sc = SparkContext('local')  # Create spark context
-spark = SparkSession(sc)  # Create session
-df = spark.read.options(delimiter='\t').csv(path)  # load csv
+class PSTool:
+    def pyspark_session(self, host_location):
+        """
+        Creates and returns spark session object
+        """
+        sc = SparkContext(host_location)  # Create spark context
+        spark = SparkSession(sc)  # Create session
+        return spark
 
-df.show()  # shows dataframe(Limited to first 20)
-df.printSchema()  # Shows column names and some info
+    def file_loader(self, path, delim, spark_obj):
+        data = spark_obj.read.options(delimiter=delim).csv(path)  # load file
+        return data
+
+
+if __name__ == "__main__":
+    # Instanciate object
+    pstool = PSTool()
+    # start session
+    spk = pstool.pyspark_session('local')
+    # load data
+    path = '/data/dataprocessing/interproscan/all_bacilli.tsv'
+    df=pstool.file_loader(path, '\t', spk)
+
+    df.take(100).write.csv('test.csv', sep='\t', header=False, mode='overwrite')
+    # df_sub.show()
+    # df.printSchema()  # Shows column names and some info
