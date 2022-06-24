8127f5affc4d94c8913d6af83b13371fc98096e3
Martin Aimé Schepers <81292024+mschepers188@users.noreply.github.com>
2022-06-24 12:53:26 +0200

    atcsvstep
    Almost done

--- Assignment5/assignment5.py
+++ Assignment5/assignment5.py
@@ -1,19 +1,27 @@
 from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
-
+from pyspark.sql.functions import mean, abs
+from collections import Counter
+import pyspark.sql.functions as f
+from pyspark.sql.types import FloatType
 
 class PSTool:
     def pyspark_session(self, host_location):
         """
         Creates and returns spark session object
         """
-        sc = SparkContext(host_location)  # Create spark context
+        sc = SparkContext(host_location[16])  # Create spark context
         spark = SparkSession(sc)  # Create session
         return spark
 
     def file_loader(self, path, delim, spark_obj):
         data = spark_obj.read.options(delimiter=delim).csv(path)  # load file
         return data
+
+    def questions(self, df):
+
+
+        # Getting everything together and writing to file
 
 
 if __name__ == "__main__":
@@ -25,6 +33,6 @@ if __name__ == "__main__":
     path = '/data/dataprocessing/interproscan/all_bacilli.tsv'
     df=pstool.file_loader(path, '\t', spk)
 
-    df.take(100).write.csv('test.csv', sep='\t', header=False, mode='overwrite')
     # df_sub.show()
     # df.printSchema()  # Shows column names and some info
+
